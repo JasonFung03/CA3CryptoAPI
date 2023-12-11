6ea906e175dcf4957fe98854de6b6b2be3582031
Jason Fung <x00174641@mytudublin.ie>
2023-12-11 20:11:06 +0000

    Added 2 tests

--- PlaywrightTests/UnitTest1.cs
+++ PlaywrightTests/UnitTest1.cs
@@ -7,36 +7,23 @@ using System.Text.RegularExpressions;
 public class Tests : PageTest
 {
     [Test]
-    public async Task MyTest()
+    public async Task CheckTopCoinButtonClickAndRedirect()
     {
         await Page.GotoAsync("https://jasonfung03.github.io/CA3CryptoAPI/");
-
         await Page.GetByRole(AriaRole.Row, new() { Name = "Bitcoin" }).GetByRole(AriaRole.Button).ClickAsync();
-
         await Expect(Page).ToHaveTitleAsync(new Regex("bitcoin"));
-
-        // var max_supply = Page.Locator("#max_supply");
-        // await Expect(max_supply).ToHaveTextAsync("21000000.0");
-
-        // await Page.GetByText("21000000.0").ClickAsync();
-
-        // await Page.GotoAsync("https://jasonfung03.github.io/CA3CryptoAPI/");
-
-        // await Page.GetByPlaceholder("Enter a coin...").ClickAsync();
-
-        // await Page.GetByPlaceholder("Enter a coin...").PressAsync("CapsLock");
-
-        // await Page.GetByPlaceholder("Enter a coin...").FillAsync("B");
-
-        // await Page.GetByPlaceholder("Enter a coin...").PressAsync("CapsLock");
-
-        // await Page.GetByPlaceholder("Enter a coin...").FillAsync("Bitcoin");
-
-        // await Page.GetByRole(AriaRole.Button, new() { Name = "Search" }).ClickAsync();
-
-        // await Page.GetByRole(AriaRole.Row, new() { Name = "Bitcoin View More Details", Exact = true }).GetByRole(AriaRole.Button).ClickAsync();
-
-        // await Page.GetByRole(AriaRole.Heading, new() { Name = "Bitcoin" }).ClickAsync();
-
+        var max_supply = Page.Locator("#max_supply");
+        await Expect(max_supply).ToHaveTextAsync("21000000.0");
+    }
+    
+    [Test]
+    public async Task TestSearchFunction() {
+        await Page.GotoAsync("https://jasonfung03.github.io/CA3CryptoAPI/");
+        await Page.GetByPlaceholder("Enter a coin...").FillAsync("Eth");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Search" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Row, new() { Name = "Ethereum View More Details", Exact = true }).GetByRole(AriaRole.Button).ClickAsync();
+        await Expect(Page).ToHaveTitleAsync(new Regex("ethereum"));
+        var max_supply = Page.Locator("#max_supply");
+        await Expect(max_supply).ToHaveTextAsync("");
     }
 }
